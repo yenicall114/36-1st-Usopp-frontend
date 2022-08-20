3794b194bbb6860402bdc93670c6f8bd2ee383a5
소재현 <socowjh@gmail.com>
2022-08-20 18:33:18 +0900

    product브랜치생성

---
+++ src/pages/Product/ProductParfum.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'ProductParfum.scss';
+
+const ProductParfum = () => {
+  return (
+    <div className="productParfum">
+      <div />
+      <div />
+    </div>
+  );
+};
+export default ProductParfum;
