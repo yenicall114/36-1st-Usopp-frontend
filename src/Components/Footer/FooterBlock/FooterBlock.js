09bc410e8bd1257121f93e1c25b60d18a3878d7f
kimjunho <jh241093@gmail.com>
2022-08-22 19:12:03 +0900

    footer UI 구현

---
+++ src/Components/Footer/FooterBlock/FooterBlock.js
@@ -0,0 +1,18 @@
+import './FooterBlock.scss';
+
+const FooterBlock = ({ data }) => {
+  const { className, title, list } = data;
+
+  return (
+    <div className={className}>
+      <h3>{title}</h3>
+      <ul>
+        {list.map((contents, idx) => (
+          <li key={idx}>{contents}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FooterBlock;
